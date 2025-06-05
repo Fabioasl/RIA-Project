ec055f44e658a9827bfa5cc16a66ae269302f767
Fábio Alexandre <163214715+Fabioasl@users.noreply.github.com>
2025-06-05 20:35:51 +0000

    feat: adiciona componentes para testes no projeto

--- Vital/src/app/app.component.ts
+++ Vital/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNG } from 'primeng/config';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-root, button-demo',
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Vital';
+  }
+
+export class ButtonDemo {
+  isbuttondisabled = false;
+  buttonClickedMessage = '';
+  handleClick(){
+    this.buttonClickedMessage = "botão clicado."
+  }
+
 }

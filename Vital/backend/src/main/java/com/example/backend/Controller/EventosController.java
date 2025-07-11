a6e3b86c1d96787b9d92408135d5c375ee5a2be4
Fábio Alexandre <163214715+Fabioasl@users.noreply.github.com>
2025-07-11 17:39:06 +0000

    fix: conserta erro de CORS

--- Vital/backend/src/main/java/com/example/backend/Controller/EventosController.java
+++ Vital/backend/src/main/java/com/example/backend/Controller/EventosController.java
@@ -22,27 +22,28 @@ public class  EventosController{
     public EventosController(EventosService eventosService){
             this.eventosService = eventosService;
     }
-
+    @CrossOrigin(origins="https://improved-robot-x5rp55rq6xgg3pq94-4200.app.github.dev")
     @PostMapping
     public ResponseEntity<Eventos> postEventos(@RequestBody Eventos evento){
         return ResponseEntity.ok(eventosService.postEventos(evento));
     }
-
+    @CrossOrigin(origins="https://improved-robot-x5rp55rq6xgg3pq94-4200.app.github.dev")
     @GetMapping
     public ResponseEntity<List<Eventos>> getEventos(){
         return ResponseEntity.ok(eventosService.readEventos());
     }
-
+    @CrossOrigin(origins="https://improved-robot-x5rp55rq6xgg3pq94-4200.app.github.dev")
     @GetMapping("/{id}")
     public ResponseEntity<Eventos> getEventos(@PathVariable Long id) {
     return eventosService.getEventosById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
+    @CrossOrigin(origins="https://improved-robot-x5rp55rq6xgg3pq94-4200.app.github.dev")
     @PutMapping("/{id}")
     public ResponseEntity<Eventos> putEventos(@RequestBody Eventos evento,@PathVariable Long id){
         return ResponseEntity.ok(eventosService.putEventos(evento, id));
     }
-
+    @CrossOrigin(origins="https://improved-robot-x5rp55rq6xgg3pq94-4200.app.github.dev")
     @DeleteMapping("/{id}")
         public ResponseEntity<Void> deleteEventos(@PathVariable Long id){
             eventosService.deleteEventos(id);
